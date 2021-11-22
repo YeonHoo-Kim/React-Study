ef6d9ccf1b6fe52270af0ea4cb2a8e33c38b7a2a
YeonHoo Kim <yeonhoo_kim@tmax.co.kr>
2021-11-22 17:47:38 +0900

    useMemo practice

--- src/examples/hooks/index.js
+++ src/examples/hooks/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import UseState from './UseState';
 import UseEffect from './UseEffect';
+import UseMemo from './UseMemo';
 
 const Hooks = () => {
   return (
     <div>
       <h2>Hooks!!!</h2>
       {/* <UseState /> */}
-      <UseEffect />
+      {/* <UseEffect /> */}
+      <UseMemo />
     </div>
   );
 };
